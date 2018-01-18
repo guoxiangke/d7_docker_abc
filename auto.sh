142e6db296a5f2f4451db0844a0e8bbdec19db4d
= <=>
2018-01-18 17:08:30 +0800

    k

--- auto.sh
+++ auto.sh
@@ -3,5 +3,5 @@ pwd
 git pull
 git push
 ssh root@123.207.178.25<< EOF
-cd /root/html/d7_docker_abc && git checkout && git pull && ./run.sh
+cd /root/html/abc && git checkout && git pull && ./run.sh
 EOF
